99ed541f04c861ef3d96ed7c4a0ae69f093648cb
LeahJKH <leahjb06.kodehode@gmail.com>
2025-04-01 10:18:02 +0200

    made it ready for when i switch too api

--- js/app.js
+++ js/app.js
@@ -1,22 +1,25 @@
-import langC from "./json/langC.json"
+fetch("./json/langC.json")
+  .then((res) => res.json())
+  .then((langC) => {
+    const langCont = document.querySelector("#language-cont");
 
-const langCont = document.querySelector("#language-cont")
+    langC.forEach((e) => {
+      const divLan = document.createElement("div");
+      const imgLan = document.createElement("img");
+      const pLan = document.createElement("p");
+      const pTxtLan = document.createTextNode(e.txt);
 
+      imgLan.src = e.img;
+      imgLan.alt = e.alt;
+      imgLan.classList.add("icon-image");
 
-langC.forEach((e) => {
-    console.log(e.txt)
-    const divLan = document.createElement("div")
-    const imgLan = document.createElement("img")
-    const pLan = document.createElement("p")
-    const pTxtLan = document.createTextNode(e.txt)
-    imgLan.src = e.img
-    imgLan.alt = e.alt
-    imgLan.classList.add("icon-image")
-    pLan.classList.add("icon-txt")
-    divLan.classList.add("lang-hold-div")
+      pLan.classList.add("icon-txt");
+      divLan.classList.add("lang-hold-div");
 
-    pLan.append(pTxtLan)
-    divLan.appendChild(imgLan)
-    divLan.appendChild(pLan)
-    langCont.appendChild(divLan)
-})
+      pLan.append(pTxtLan);
+      divLan.appendChild(imgLan);
+      divLan.appendChild(pLan);
+      langCont.appendChild(divLan);
+    });
+  })
+  .catch((err) => console.error("Failed to load JSON:", err));
