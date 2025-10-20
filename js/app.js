a6b036dd511b6cd4a0558eb74acf451020342fb1
LeahJKH <Leahjb06.kodehode@gmail.com>
2025-10-20 14:16:54 +0200

    switched over too json fully

--- js/app.js
+++ js/app.js
@@ -36,75 +36,46 @@ setInterval(spawnStar, 800);
 setInterval(spawnStar, 800);
 
 const cont = document.querySelector("#projects-sect");
+async function getData() {
 
-const projects = [
-  {
-    img: "./images/projects/JobScraper.webp",
-    alt: "A screenshot of the code in python",
-    header: "JobScraper",
-    desc: "Denne var laget for og søke etter jobber med minnimalt arbeid for markeds teamet",
-    link: "https://github.com/LeahJKH/pyScraper",
-  },
-  {
-    img: "./images/projects/Hometown.webp",
-    alt: "a picture of the frontpage for a site about telavåg",
-    header: "My hometown",
-    desc: "Denne var laget for og vise fram bilder jeg hadde tatt av min hjemby",
-    link: "https://github.com/LeahJKH/my-hometown",
-  },
-  {
-    img: "./images/projects/Go-Calc.webp",
-    alt: "picture of a dark themed simple calculator",
-    header: "Go-Calc",
-    desc: "Her testet jeg Go Lang og lagde en simpel kalkulator",
-    link: "https://github.com/LeahJKH/Go-calc",
-  },
-  {
-    img: "./images/projects/CheatSheet.webp",
-    alt: "a screen dump of the page with some foundational Javascript",
-    header: "Coding Cheatsheet",
-    desc: "this is made for fun just a cheat sheet for coding langs around web dev",
-    link: "https://github.com/LeahJKH/Coding-Cheatsheet"
-  }
-];
+  const res = await fetch("https://raw.githubusercontent.com/LeahJKH/Portifolio/refs/heads/main/json/projects/all.json")
+  let data = await res.json()
 
-projects.forEach((e) => {
-  const mainDiv = document.createElement("div");
-  mainDiv.classList.add("projcard", "column");
+  data.forEach((e) => {
+    const mainDiv = document.createElement("div");
+    mainDiv.classList.add("projcard", "column");
 
-  const img = document.createElement("img");
-  img.classList.add("projImg");
-  img.src = e.img;
-  img.alt = e.alt;
+    const img = document.createElement("img");
+    img.classList.add("projImg");
+    img.src = e.img;
+    img.alt = e.alt;
 
-  const divider = document.createElement("div");
-  divider.classList.add("info-cont-div");
-  const mainHead = document.createElement("h2");
-  mainHead.classList.add("projHeading", "highlighted-text");
-  let textHead = document.createTextNode(e.header);
-  mainHead.append(textHead);
+    const divider = document.createElement("div");
+    divider.classList.add("info-cont-div");
+    const mainHead = document.createElement("h2");
+    mainHead.classList.add("projHeading", "highlighted-text");
+    let textHead = document.createTextNode(e.header);
+    mainHead.append(textHead);
 
-  const p = document.createElement("p");
-  const ptxt = document.createTextNode(e.desc);
-  p.append(ptxt);
+    const p = document.createElement("p");
+    const ptxt = document.createTextNode(e.desc);
+    p.append(ptxt);
 
-  const btn = document.createElement("button");
-  btn.classList.add("btnproj");
-  const btnTxt = document.createTextNode("GO");
-  const a = document.createElement("a")
-  a.href = e.link
-  a.target = "_blank"
-  a.append(btnTxt)
-  btn.append(a)
+    const btn = document.createElement("button");
+    btn.classList.add("btnproj");
+    const btnTxt = document.createTextNode("GO");
+    const a = document.createElement("a")
+    a.href = e.link
+    a.target = "_blank"
+    a.append(btnTxt)
+    btn.append(a)
 
-  mainDiv.appendChild(img);
-  divider.appendChild(mainHead);
-  divider.appendChild(p);
-  divider.appendChild(btn);
-  mainDiv.appendChild(divider);
-  cont.appendChild(mainDiv);
-});
-
-
-
-
+    mainDiv.appendChild(img);
+    divider.appendChild(mainHead);
+    divider.appendChild(p);
+    divider.appendChild(btn);
+    mainDiv.appendChild(divider);
+    cont.appendChild(mainDiv);
+  });
+}
+getData()
